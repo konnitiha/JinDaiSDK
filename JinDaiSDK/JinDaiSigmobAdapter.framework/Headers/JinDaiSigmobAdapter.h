d834d69abae6f538e7616083f20b338f2574f0cb
fyl <1053591151@qq.com>
2026-05-18 23:30:14 +0800

    1.1.0

---
+++ JinDaiSDK/JinDaiSigmobAdapter.framework/Headers/JinDaiSigmobAdapter.h
@@ -0,0 +1,18 @@
+//
+//  JinDaiSigmobAdapter.h
+//  JinDaiSigmobAdapter
+//
+//  Created by fyl on 2025/7/1.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for JinDaiSigmobAdapter.
+FOUNDATION_EXPORT double JinDaiSigmobAdapterVersionNumber;
+
+//! Project version string for JinDaiSigmobAdapter.
+FOUNDATION_EXPORT const unsigned char JinDaiSigmobAdapterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JinDaiSigmobAdapter/PublicHeader.h>
+
+
